a883d9a0faba495a846ad0067b4ca1f0dc45c2c3
yra <ymz.ncnk@outlook.com>
2026-05-06 17:37:05 +0100

    docs: update WithMaxLen documentation to clarify its purpose and return type

--- options.go
+++ options.go
@@ -5,7 +5,8 @@ import cdc "github.com/cmd-stream/codec-go"
 // SetOption is a function that sets an option for the codec.
 type SetOption = cdc.SetOption
 
-// WithMaxLen sets the maximum length of the byte slice.
+// WithMaxLen returns a SetOption that sets the maximum length of the encoded
+// message byte slice.
 func WithMaxLen(maxLen int) SetOption {
 	return cdc.WithMaxLen(maxLen)
 }
